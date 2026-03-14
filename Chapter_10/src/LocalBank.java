b0ab939571a99790a5d20d5c0ac52602fa0c6dee
Quang041 <nguyenquangn68@gmail.com>
2026-03-14 15:59:38 -0600

    3:59 PM

--- Chapter_10/src/LocalBank.java
+++ Chapter_10/src/LocalBank.java
@@ -3,7 +3,6 @@ import javax.swing.JFrame;
 import javax.swing.JComboBox;
 import java.awt.BorderLayout;
 import java.awt.Color;
-
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.DefaultComboBoxModel;
@@ -11,12 +10,11 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.JTextArea;
 public class LocalBank {
-
 	private JFrame frame;
-	private JTextField UserInput_AccountNum;
-	private JTextField UserInput_Withdraw_Deposit;
+	private JTextField Num;
+	private JTextField Input;
 	private JTextField FName;
 	private JTextField LName;
 	private JTextField Starting_Num;
@@ -29,7 +27,6 @@ public class LocalBank {
 	
 	private Bank bank = new Bank();
 	
-
 	/**
 	 * Launch the application.
 	 */
@@ -45,14 +42,12 @@ public class LocalBank {
 			}
 		});
 	}
-
 	/**
 	 * Create the application.
 	 */
 	public LocalBank() {
 		initialize();
 	}
-
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -62,13 +57,99 @@ public class LocalBank {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		
+		/**
+		 * User's information
+		 */
+		
+		JTextArea Information = new JTextArea();
+		Information.setLineWrap(true);
+		Information.setBounds(24, 333, 387, 115);
+		frame.getContentPane().add(Information);
+		
+		/** User Input/ User interaction**/
+		
+		Num = new JTextField();
+		Num.setBounds(24, 139, 387, 20);
+		frame.getContentPane().add(Num);
+		Num.setColumns(10);
+		
+		Input = new JTextField();
+		Input.setColumns(10);
+		Input.setBounds(24, 178, 387, 20);
+		frame.getContentPane().add(Input);
+		
+		FName = new JTextField();
+		FName.setColumns(10);
+		FName.setBounds(24, 216, 387, 20);
+		frame.getContentPane().add(FName);
+		
+		LName = new JTextField();
+		LName.setColumns(10);
+		LName.setBounds(24, 257, 387, 20);
+		frame.getContentPane().add(LName);
+		
+		Starting_Num = new JTextField();
+		Starting_Num.setColumns(10);
+		Starting_Num.setBounds(24, 298, 387, 20);
+		frame.getContentPane().add(Starting_Num);
+		
+		/** Guidance **/
+		
+		guidance = new JLabel("Complete the information in RED");
+		guidance.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		guidance.setBounds(24, 91, 387, 26);
+		frame.getContentPane().add(guidance);
+		
+		JLabel Option = new JLabel("Section an option");
+		Option.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		Option.setBounds(24, 23, 387, 26);
+		frame.getContentPane().add(Option);
+		
+		last_name = new JLabel("Last Name:");
+		last_name.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		last_name.setBounds(24, 232, 387, 26);
+		frame.getContentPane().add(last_name);
+		
+		first_balance = new JLabel("Beginning Balance:");
+		first_balance.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		first_balance.setBounds(24, 274, 387, 26);
+		frame.getContentPane().add(first_balance);
+		
+		Money_flow = new JLabel("Amount of Withdraw/Deposit:");
+		Money_flow.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		Money_flow.setBounds(24, 157, 387, 26);
+		frame.getContentPane().add(Money_flow);
+		
+		first_name = new JLabel("First Name:");
+		first_name.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		first_name.setBounds(24, 199, 387, 16);
+		frame.getContentPane().add(first_name);
+		
+		Account_num = new JLabel("Account Number:");
+		Account_num.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		Account_num.setBounds(24, 115, 387, 31);
+		frame.getContentPane().add(Account_num);
+		
 		JComboBox Options = new JComboBox();
-		Options.setModel(new DefaultComboBoxModel(new String[] {"", "Create New Account", "Delete Account", "Deposit", "Withdraw"}));
+		Options.setModel(new DefaultComboBoxModel(new String[] {"", "Create New Account", "Delete Account", "Deposit", "Withdraw", "Check Balance"}));
 		Options.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) 
+			public void actionPerformed(ActionEvent e)
 			{
 				
+				Information.setText(" ");
+				
 				if (Options.getSelectedItem().equals("Create New Account")) {
+					FName.setText("");
+					LName.setText("");
+					Starting_Num.setText("");
+					
+					FName.setVisible(true);
+					LName.setVisible(true);
+					Starting_Num.setVisible(true);
+					Num.setVisible(false);
+					Input.setVisible(false);
+					
 					first_name.setForeground(Color.RED);
 					first_name.setText("First Name: *");
 					last_name.setForeground(Color.RED);
@@ -81,18 +162,36 @@ public class LocalBank {
 					Money_flow.setText("Amount of Withdraw/Deposit:");
 				}
 				else if (Options.getSelectedItem().equals("Delete Account")) {
-					first_name.setForeground(Color.RED);
-					first_name.setText("First Name: *");
-					last_name.setForeground(Color.RED);
-					last_name.setText("Last Name: *");
-					first_balance.setForeground(Color.RED);
-					first_balance.setText("Beginning Balance: *");
-					Account_num.setForeground(Color.BLACK);
-					Account_num.setText("Account Number: ");
+					Num.setText("");
+					Input.setText("");
+					
+					FName.setVisible(false);
+					LName.setVisible(false);
+					Starting_Num.setVisible(false);
+					Num.setVisible(true);
+					Input.setVisible(false);
+					
+					first_name.setForeground(Color.BLACK);
+					first_name.setText("First Name:");
+					last_name.setForeground(Color.BLACK);
+					last_name.setText("Last Name:");
+					first_balance.setForeground(Color.BLACK);
+					first_balance.setText("Beginning Balance:");
+					Account_num.setForeground(Color.RED);
+					Account_num.setText("Account Number: *");
 					Money_flow.setForeground(Color.BLACK);
 					Money_flow.setText("Amount of Withdraw/Deposit: ");
 				}
 				else if (Options.getSelectedItem().equals("Deposit")) {
+					Num.setText("");
+					Input.setText("");
+					
+					FName.setVisible(false);
+					LName.setVisible(false);
+					Starting_Num.setVisible(false);
+					Num.setVisible(true);
+					Input.setVisible(true);
+					
 					first_name.setForeground(Color.BLACK);
 					first_name.setText("First Name:");
 					last_name.setForeground(Color.BLACK);
@@ -106,6 +205,15 @@ public class LocalBank {
 					
 				}
 				else if (Options.getSelectedItem().equals("Withdraw")) {
+					Num.setText("");
+					Input.setText("");
+					
+					FName.setVisible(false);
+					LName.setVisible(false);
+					Starting_Num.setVisible(false);
+					Num.setVisible(true);
+					Input.setVisible(true);
+					
 					first_name.setForeground(Color.BLACK);
 					first_name.setText("First Name:");
 					last_name.setForeground(Color.BLACK);
@@ -117,88 +225,161 @@ public class LocalBank {
 					Money_flow.setForeground(Color.RED);
 					Money_flow.setText("Amount of Withdraw: *");
 				}
+				else if (Options.getSelectedItem().equals("Check Balance")) {
+					Num.setText("");
+					Input.setText("");
+					
+					FName.setVisible(false);
+					LName.setVisible(false);
+					Starting_Num.setVisible(false);
+					Num.setVisible(true);
+					Input.setVisible(false);
+					
+					first_name.setForeground(Color.BLACK);
+					first_name.setText("First Name:");
+					last_name.setForeground(Color.BLACK);
+					last_name.setText("Last Name:");
+					first_balance.setForeground(Color.BLACK);
+					first_balance.setText("Beginning Balance:");
+					Account_num.setForeground(Color.RED);
+					Account_num.setText("Account Number: *");
+					Money_flow.setForeground(Color.BLACK);
+					Money_flow.setText("Amount of Withdraw:");
+				}
 	}
 		});
 		Options.setBounds(24, 48, 387, 22);
 		frame.getContentPane().add(Options);
-		
-		
-		
-		/** User Input/ User interaction**/
-		
-		UserInput_AccountNum = new JTextField();
-		UserInput_AccountNum.setBounds(24, 164, 387, 20);
-		frame.getContentPane().add(UserInput_AccountNum);
-		UserInput_AccountNum.setColumns(10);
-		
-		UserInput_Withdraw_Deposit = new JTextField();
-		UserInput_Withdraw_Deposit.setColumns(10);
-		UserInput_Withdraw_Deposit.setBounds(24, 206, 387, 20);
-		frame.getContentPane().add(UserInput_Withdraw_Deposit);
-		
-		FName = new JTextField();
-		FName.setColumns(10);
-		FName.setBounds(24, 250, 387, 20);
-		frame.getContentPane().add(FName);
-		
-		LName = new JTextField();
-		LName.setColumns(10);
-		LName.setBounds(24, 293, 387, 20);
-		frame.getContentPane().add(LName);
-		
-		Starting_Num = new JTextField();
-		Starting_Num.setColumns(10);
-		Starting_Num.setBounds(24, 339, 387, 20);
-		frame.getContentPane().add(Starting_Num);
-		
-		/** Guidance **/
-		
-		guidance = new JLabel("Complete the information in RED");
-		guidance.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		guidance.setBounds(24, 112, 387, 26);
-		frame.getContentPane().add(guidance);
-		
-		JLabel Option = new JLabel("Section an option");
-		Option.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		Option.setBounds(24, 23, 387, 26);
-		frame.getContentPane().add(Option);
-		
-		last_name = new JLabel("Last Name:");
-		last_name.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		last_name.setBounds(24, 272, 387, 26);
-		frame.getContentPane().add(last_name);
-		
-		first_balance = new JLabel("Beginning Balance:");
-		first_balance.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		first_balance.setBounds(24, 318, 387, 26);
-		frame.getContentPane().add(first_balance);
-		
-		Money_flow = new JLabel("Amount of Withdraw/Deposit:");
-		Money_flow.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		Money_flow.setBounds(24, 185, 387, 26);
-		frame.getContentPane().add(Money_flow);
-		
-		first_name = new JLabel("First Name:");
-		first_name.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		first_name.setBounds(24, 234, 387, 16);
-		frame.getContentPane().add(first_name);
-		
-		Account_num = new JLabel("Accound Number:");
-		Account_num.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		Account_num.setBounds(24, 140, 387, 31);
-		frame.getContentPane().add(Account_num);
-		
+
+		/** 
+		 * DONE ALL THINGS
+		 */
 		JButton Check = new JButton("Process Transaction");
 		Check.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		Check.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				
+				String fname = FName.getText();
+				String lname = LName.getText();
+				String Security = Num.getText();
+				
+				
+				if (Options.getSelectedItem().equals("Create New Account")) {
+					
+					if (fname == null || lname == null || Starting_Num.getText().isEmpty()) {
+						Information.setText("Cannot process");
+					}
+					else {
+						fname = FName.getText();
+						lname = LName.getText();
+						
+						double num = Double.parseDouble(Starting_Num.getText());
+						String new_acct = bank.addAccount(fname, lname, num);
+						Information.setText(" New Account Created Successfully!\n" +
+						"----------------------------------------------------------------------------------------------\n" +
+						"First Name: " + fname + ".\n" +
+						"Last Name: " + lname + ".\n" +
+						"Personal ID: " + new_acct + " - Please DO NOT lose this.\n" +
+						"----------------------------------------------------------------------------------------------\n" +
+						"Current Balance: $" + num);
+					}
+				}
+				else if (Options.getSelectedItem().equals("Delete Account")) {
+					
+					if (Security == null) {
+						Information.setText("Cannot procees");
+					}
+					else {
+						String guard = bank.deleteAccount(Security);
+						Information.setText(guard);
+					}
+					
+				}
+				else if (Options.getSelectedItem().equals("Deposit")) {
+					
+					
+					/** Input = double, Security = String **/
+					if (Input.getText().isEmpty() || Security == null) {
+						Information.setText("Cannot procees this transaction.\n" +
+						"Please make sure you enter all the data.");
+					}
+					else {
+						double num = Double.parseDouble(Input.getText());
+						String dep = bank.transaction(1, Security, num);
+						
+						if (dep.equals("Account does not exist.")) {
+							Information.setText(dep + "\n" +
+							"----------------------------------------------------------------------------------------------\n" +
+							"Please make sure checking the space/capital letters in your personal ID\n" + 
+							"----------------------------------------------------------------------------------------------\n");
+						}
+						else {
+							String balance = dep.substring(dep.indexOf("Current balance is"));
+							String accountInfo = dep.substring(0, dep.indexOf("Current")).trim();
+							
+							Information.setText("Receipt.\n" +
+							"-----------------------------------------------------------------------------------------------\n" +
+							"User Name: " + accountInfo.substring(accountInfo.indexOf(" ")).trim()+ ".\n" +
+							"Transaction Type: Deposit.\n" +
+							"Amount of Deposit: " + num + ".\n" +
+							"-----------------------------------------------------------------------------------------------\n" +
+							balance);
+						}
+					}
+				}
+				else if (Options.getSelectedItem().equals("Withdraw")) {
+
+					
+					/** Input = double, Security = String **/
+					if (Input.getText().isEmpty() || Security == null) {
+						Information.setText("Cannot procees this transaction.\n" +
+						"Please make sure you enter all the data.");
+					}
+					else {
+						double num = Double.parseDouble(Input.getText());
+						String dep = bank.transaction(1, Security, num);
+						
+						if (dep.equals("Account does not exist.")) {
+							Information.setText(dep + "\n" +
+							"----------------------------------------------------------------------------------------------\n" +
+							"Please make sure checking the space/capital letters in your personal ID\n" + 
+							"----------------------------------------------------------------------------------------------\n");
+						}
+						else {
+							String balance = dep.substring(dep.indexOf("Current balance is"));
+							String accountInfo = dep.substring(0, dep.indexOf("Current")).trim();
+							
+							Information.setText("Receipt.\n" +
+							"-----------------------------------------------------------------------------------------------\n" +
+							"User Name: " + accountInfo.substring(accountInfo.indexOf(" ")).trim()+ ".\n" +
+							"Transaction Type: Withdraw.\n" +
+							"Amount of Deposit: " + num + ".\n" +
+							"-----------------------------------------------------------------------------------------------\n" +
+							balance);
+						}
+					}
+				}
+				else if (Options.getSelectedItem().equals("Check Balance")) {
+					
+					
+					if (Security == null) {
+						Information.setText("Cannot process");
+					}
+					else {
+						String check = bank.checkBalance(Security);
+						String balance = check.substring(check.indexOf("Current balance is"));
+						Information.setText("Account Check.\n" +
+						"-----------------------------------------------------------------------------------------------\n" +
+						"First Name: " + fname + ".\n" +
+						"Last Name: " + lname + ".\n" + 
+						"-----------------------------------------------------------------------------------------------\n" +
+						balance);
+					}
+					}
+		}
+		});
 		Check.setBounds(19, 465, 182, 31);
 		frame.getContentPane().add(Check);
-		
-		/** User Response **/
-		
-		JLabel Information = new JLabel("");
-		Information.setBounds(24, 370, 387, 84);
-		frame.getContentPane().add(Information);
-		
 	}	
 }
-		

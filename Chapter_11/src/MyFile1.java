e6cd335327b3ac0f448d3c66b11a3e36af44f2e2
1100101825 <1100101825@L815WD220022.edu.cbe.ab.ca>
2026-03-17 14:52:17 -0600

    2:52 PM

---
+++ Chapter_11/src/MyFile1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class MyFile1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner userinput = new Scanner(System.in);
+		
+		String file = userinput.nextLine();
+		String file1 = file.replace("\\", "\\\\");
+		
+		File textFile = new File(file);
+		
+		if (textFile.exists() ) {
+			System.out.println("The file is exist");
+		}
+		else {
+			System.out.println("The file is not exist");
+		}
+	}
+}

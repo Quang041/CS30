ea357be82b93a15fc79d35abe7a0fb52e8e9cea0
1100101825 <1100101825@L815WD220022.edu.cbe.ab.ca>
2026-03-31 15:37:53 -0600

    3:37 PM

--- Chapter_11/src/SkillBuilder/WordGuess.java
+++ Chapter_11/src/SkillBuilder/WordGuess.java
@@ -2,9 +2,34 @@ package SkillBuilder;
 
 import java.util.*;
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class WordGuess {
 	public static void main(String[] args) {
-		System.out.print("dfjhjsdhfjsdg");
-	}
-}
+		
+		
+		System.out.print("How to play\n" +
+		"------------------------------\n" +
+		"* Just guess the word I give you.\n" +
+		"P/S: It's not that hard, trust");
+		
+		String textFile = ("../Chapter_11/src/Assignment_Something_IDK.txt");
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(textFile))) {
+			String count;
+			while ((reader.readLine() != null)) {
+				
+			}
+		} catch (IOException e) {
+			System.out.println("An error occured while reading the file: " + e.getMessage());
+			e.printStackTrace();
+		}
+	
+
+}}
+
+
+

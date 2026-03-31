e7adda67e8bee8fd3471a04e4a4ecd73becc45c7
1100101825 <1100101825@L815WD220022.edu.cbe.ab.ca>
2026-03-31 14:46:46 -0600

    2:46 PM

--- Chapter_11/src/SkillBuilder/WordGuess.java
+++ Chapter_11/src/SkillBuilder/WordGuess.java
@@ -1,10 +1,10 @@
+package SkillBuilder;
+
 import java.util.*;
 import java.io.*;
 
-
-public class Stats1 {
+public class WordGuess {
 	public static void main(String[] args) {
-		
+		System.out.print("dfjhjsdhfjsdg");
 	}
-	
 }

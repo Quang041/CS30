598c9e594d021033e387a2dd87c1bedfcda380e3
Quang041 <nguyenquangn68@gmail.com>
2026-04-07 14:20:55 -0600

    2:20 PM

--- Chapter_11/src/SkillBuilder/CountVowels.java
+++ Chapter_11/src/SkillBuilder/CountVowels.java
@@ -23,27 +23,26 @@ public class CountVowels {
 		
 		Scanner userinput = new Scanner(System.in);
 		System.out.print("Please enter you file name: ");
-		String path = userinput.nextLine();
+		String path = userinput.nextLine(); //User input//
 		
 		
-		String lineOfText;
-		int count = 0;
-		String vowel = "eauio";
+		String lineOfText; //Use to store the text in the file//
+		int count = 0;  //Count the amount of vowels//
+		String vowel = "eauio";  //What are vowels include//
 		
-		File fileText = new File(path);
+		File fileText = new File(path);  //Take access to the file//
 		
-		try (BufferedReader buffer = new BufferedReader(new FileReader(fileText))) {
-			while ((lineOfText = buffer.readLine()) != null) {
-				for (char calen: lineOfText.toLowerCase().toCharArray()) {
-					String alinda = Character.toString(calen);
-					if (vowel.contains(alinda)) {
+		try (BufferedReader buffer = new BufferedReader(new FileReader(fileText))) {  //Read the file//
+			while ((lineOfText = buffer.readLine()) != null) { //Condition: if the line from the text, sent to lineOfText is not empty, continue in the for loop//
+				for (char calen: lineOfText.toLowerCase().toCharArray()) { //Take ever characters from the text, convert to lowercase//
+					String alinda = Character.toString(calen); //Convert the char data type to String type, in order to compare//
+					if (vowel.contains(alinda)) { //If any vowel is found, update the count variable//
 						count++;
 					}
 				}
 			}      
-			System.out.println("Your file has: " + count + " vowel(s).");
-			System.out.println(count);
-		} catch (IOException e) {
+			System.out.println("Your file has: " + count + " vowel(s)."); //Notify how any vowels//
+		} catch (IOException e) { //Catch any error if the file cannot be read//
 			System.out.println("Problem reading file.");
 			System.out.println("IOException: " + e.getMessage());
 		}	
